31cf46c3b1a3638564edd07d111fb15c53ee24fb
silvermistshadow <bmead72@gmail.com>
2020-03-16 10:29:37 -0700

    update code and start writing test

---
+++ ScrabbleScoreTests/ModelsTests/ScoreTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrabble.Score;
+
+namespace ScoreTests 
+{
+    [TestClass]
+    public class ScoreTests
+    {
+        [TestMethod]
+        public void Score_GetScoreOfSingleLetter_ScoreOfLetter()
+        {
+            //Arrange
+            
+            //Act
+
+            //Assert
+
+        }
+    }
+}
